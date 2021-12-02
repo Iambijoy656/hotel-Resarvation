955abfa8471a87ef121aafcc96641c6980c4add9
Iambijoy656 <freelancing.bijoy.com>
2021-12-02 23:30:19 +0600

    Hotel-Reservation

---
+++ hotel_booking (1).sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2021 at 06:28 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotel booking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checked`
+--
+
+CREATE TABLE `checked` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf16_bin NOT NULL,
+  `email` varchar(20) COLLATE utf16_bin NOT NULL,
+  `number` int(11) NOT NULL,
+  `checkin_date` datetime NOT NULL,
+  `checkout_date` datetime NOT NULL,
+  `room_type` varchar(11) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `checked`
+--
+
+INSERT INTO `checked` (`id`, `name`, `email`, `number`, `checkin_date`, `checkout_date`, `room_type`) VALUES
+(125, 'Bijoy das', 'bijoy@gmail.com', 1865800656, '2021-11-24 00:00:00', '2021-12-07 00:00:00', 'Double-room'),
+(127, 'pritom das', 'protom@gmail.com', 1865800656, '2021-11-24 00:00:00', '2021-11-30 00:00:00', 'Double-room'),
+(130, 'pritom das', 'pritom@gmail.com', 1865800656, '2021-12-02 00:00:00', '2021-12-20 00:00:00', 'Choose...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf16_bin DEFAULT NULL,
+  `email` varchar(20) COLLATE utf16_bin NOT NULL,
+  `massage` varchar(50) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `name`, `email`, `massage`) VALUES
+(1, 'sonjoy saha', 'sonjoy@gmail.com', 'dfrhh'),
+(4, 'sonjoy saha', 'sonjoy@gmail.com', 'degef'),
+(6, 'bijoy', 'bijoy@gmail.com', 'DSFbeGH'),
+(9, 'JIBON KUMAR DAS', 'jibon@gmail.com', 'that hotel is very comfortable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(15) COLLATE utf16_bin NOT NULL,
+  `code` varchar(11) COLLATE utf16_bin NOT NULL,
+  `description` varchar(100) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `code`, `description`) VALUES
+(1, 'admin', 'admin', 'admin can view everything'),
+(2, 'user', 'user', 'customers can view only customers page');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf16_bin NOT NULL,
+  `number` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf16_bin NOT NULL,
+  `password` varchar(50) COLLATE utf16_bin NOT NULL,
+  `roles_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`, `roles_id`) VALUES
+(1, 'Bijoy', 1538096527, 'bijoy@gmail.com', '12345', 1),
+(3, 'sonjoy saha', 1865800656, 'sonjoy@gmail.com', '12345', 2),
+(13, 'joy', 1865800656, 'joy@gmail.com', '12345', 2),
+(15, '`pritom das', 1865800656, 'pritom@gmail.com', '12345', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `checked`
+--
+ALTER TABLE `checked`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `checked`
+--
+ALTER TABLE `checked`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
